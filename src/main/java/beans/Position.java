c30bf67d579038a0b036f1634a6a252f95d07853
Francois <francois.georis@student.unamur.be>
2017-10-31 17:32:51 +0100

    add Position

---
+++ src/main/java/beans/Position.java
@@ -0,0 +1,14 @@
+package beans;
+
+public class Position {
+    private double lon;
+    private double lat;
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
