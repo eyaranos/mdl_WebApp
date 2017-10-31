588dad4859cedc86ad5fe8275c2a3633af3c71e1
Francois <francois.georis@student.unamur.be>
2017-10-31 17:35:47 +0100

    add Velo

---
+++ src/main/java/beans/Velo.java
@@ -0,0 +1,22 @@
+package beans;
+
+public class Velo {
+    private Position position;
+    private int id;
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
